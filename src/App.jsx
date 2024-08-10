9ccbc427c14dabe6f24acef220f465a59f5274ec
Aditya Raj <138434890+Raj-Aditya-27@users.noreply.github.com>
2024-08-10 19:29:08 +0530

    Sign up with google part done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from "react";
-import "./App.css";
 import Login from "./pages/Login";
 
 function App() {
-  return <>
-    <Login/>
-  </>;
+  return (
+    <div>
+      <Login />
+    </div>
+  );
 }
 
 export default App;
